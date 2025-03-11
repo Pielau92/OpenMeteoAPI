71969a8993021c7cc465bc18ae57dc016e5e6afb
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-03-11 13:58:21 +0100

    Create tmy2format.py
    Added information about tmy2 format as a dict (according to source: https://www.nrel.gov/docs/legosti/old/7668.pdf).

---
+++ ConvertToTM2/tmy2format.py
@@ -0,0 +1,192 @@
+# region HEADER ELEMENTS IN THE TMY2 FORMAT
+
+HEADER_ELEMENTS_POS = {
+    'wban': (2, 6),
+    'city': (8, 29),
+    'state': (31, 32),
+    'timezone': (34, 36),
+
+    'latitude': {
+        'north': 38,
+        'degrees': (40, 41),
+        'minutes': (43, 44),
+    },
+
+    'longitude': {
+        'north': 46,
+        'degrees': (48, 50),
+        'minutes': (52, 53),
+    },
+
+    'elevation': (56, 59)
+}
+
+# endregion
+
+# region DATA ELEMENTS IN THE TMY2 FORMAT
+
+DATA_ELEMENTS_POS = {
+    # local standard time
+    'year': {'value': (2, 3)},
+    'month': {'value': (4, 5)},
+    'day': {'value': (6, 7)},
+    'hour': {'value': (8, 9)},
+
+    # extraterrestrial horizontal radiation [W/m²]
+    'et_hor_rad': {'value': (10, 13)},
+
+    # extraterrestrial direct normal radiation [W/m²]
+    'et_dir_norm_rad': {'value': (14, 17)},
+
+    # global horizontal radiation [W/m²]
+    'global_hor_rad': {
+        'value': (18, 21),
+        'source_flag': 22,
+        'uncertainty_flag': 23
+    },
+
+    # direct normal radiation [W/m²]
+    'dir_norm_rad': {
+        'value': (24, 27),
+        'source_flag': 28,
+        'uncertainty_flag': 29
+    },
+
+    # diffuse horizontal radiation [W/m²]
+    'diff_hor_rad': {
+        'value': (30, 33),
+        'source_flag': 34,
+        'uncertainty_flag': 35,
+    },
+
+    # global horizontal illuminance [100 lux]
+    'global_hor_ill': {
+        'value': (36, 39),
+        'source_flag': 40,
+        'uncertainty_flag': 41,
+    },
+
+    # direct normal illuminance [100 lux]
+    'dir_norm_illuminance': {
+        'value': (42, 45),
+        'source_flag': 46,
+        'uncertainty_flag': 47,
+    },
+
+    # diffuse horizontal illuminance [100 lux]
+    'diff_hor_illuminance': {
+        'value': (48, 51),
+        'source_flag': 52,
+        'uncertainty_flag': 53,
+    },
+
+    # zenith luminance [10 Cd/m²]
+    'zenith_illuminance': {
+        'value': (54, 57),
+        'source_flag': 58,
+        'uncertainty_flag': 59,
+    },
+
+    # total sky cover [10 %]
+    'total_sky_cover': {
+        'value': (60, 61),
+        'source_flag': 62,
+        'uncertainty_flag': 63,
+    },
+
+    # opaque sky cover [10 %]
+    'opaque_sky_cover': {
+        'value': (64, 65),
+        'source_flag': 66,
+        'uncertainty_flag': 67,
+    },
+
+    # dry bulb temperature [0.1 °C]
+    'dry_bulb_temp': {
+        'value': (68, 71),
+        'source_flag': 72,
+        'uncertainty_flag': 73,
+    },
+
+    # dew point temperature [0.1 °C]
+    'dew_point_temp': {
+        'value': (74, 77),
+        'source_flag': 78,
+        'uncertainty_flag': 79,
+    },
+
+    # relative humidity [%]
+    'rel_hum': {
+        'value': (80, 82),
+        'source_flag': 83,
+        'uncertainty_flag': 84,
+    },
+
+    # atmospheric pressure [mbar]
+    'atmos_pressure': {
+        'value': (85, 88),
+        'source_flag': 89,
+        'uncertainty_flag': 90,
+    },
+
+    # wind direction [degree], N=0
+    'wind_dir': {
+        'value': (91, 93),
+        'source_flag': 94,
+        'uncertainty_flag': 95,
+    },
+
+    # wind speed [0.1 m/s]
+    'wind_speed': {
+        'value': (96, 98),
+        'source_flag': 99,
+        'uncertainty_flag': 100,
+    },
+
+    # visibility [0.1 km]
+    'visibility': {
+        'value': (101, 104),
+        'source_flag': 105,
+        'uncertainty_flag': 106,
+    },
+
+    # ceiling height [m]
+    'ceiling_height': {
+        'value': (107, 111),
+        'source_flag': 112,
+        'uncertainty_flag': 113,
+    },
+
+    # present weather
+    'present_weather': {'value': (114, 123)},
+
+    # precipitable_water [mm]
+    'precipitable_water': {
+        'value': (124, 126),
+        'source_flag': 127,
+        'uncertainty_flag': 128,
+    },
+
+    # aerosol optical depth [0.001 day]
+    'aerosol_optical_depth': {
+        'value': (129, 131),
+        'source_flag': 132,
+        'uncertainty_flag': 133,
+    },
+
+    # snow depth [cm]
+    'snow_depth': {
+        'value': (134, 136),
+        'source_flag': 137,
+        'uncertainty_flag': 138,
+    },
+
+    # days since last snowfall [day]
+    'days_since_snowfall': {
+        'value': (139, 140),
+        'source_flag': 141,
+        'uncertainty_flag': 142,
+    }
+}
+
+# endregion
