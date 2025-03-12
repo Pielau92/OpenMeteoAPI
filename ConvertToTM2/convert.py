fde4f5c02009d103f3dbba74dc085a582680e85e
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-03-12 11:47:00 +0100

    Create convert.py
    - Add Line class - manages a single line inside a tm2 file
    - Add TMY2 class - manages all lines inside a tm2 file

---
+++ ConvertToTM2/convert.py
@@ -0,0 +1,36 @@
+from tmy2format import HEADER_ELEMENTS_POS, DATA_ELEMENTS_POS
+
+
+class TMY2:
+    def __init__(self, length=8760):
+        self.length = length
+        self.header = Line(HEADER_ELEMENTS_POS)
+        self.lines = [Line(DATA_ELEMENTS_POS) for _ in range(self.length)]
+
+    def write(self, data):
+
+        for line_index in range(self.length):
+            values = {}
+            for key in data:
+                values.update({key: data[key][line_index]})
+            self.lines[line_index].set_values(values)
+
+    def print(self):
+        for line in self.lines:
+            print("".join(line.line))
+
+
+class Line:
+    def __init__(self, file_format, line_len=59):
+        self.line = list(' ' * line_len)
+        self.format = file_format
+
+    def set_values(self, values):
+        for key in values:
+            self.set_value(key, values[key])
+
+    def set_value(self, parameter, value):
+        start_pos = self.format[parameter]['value'][0] - 1
+        end_pos = self.format[parameter]['value'][1]
+        self.line[start_pos:end_pos] = str(value).ljust(end_pos - start_pos)
+
