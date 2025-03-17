e0c00416aaae87fde94c689b899bc2e0ad3b9611
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-03-17 15:04:16 +0100

    Add documentation
    Add/update docstrings, type hints and explainatory comments. Also minor changes in nomenclature.

--- ConvertToTM2/convert.py
+++ ConvertToTM2/convert.py
@@ -5,15 +5,18 @@ class TMY2:
     def __init__(self, lat: float, long: float, time_zone: int, length: int = 8760) -> None:
         """Initialize tmy2 conversion.
 
+        For more information about the tmy2 format, see "The User's Manual for TMY2s" under
+        https://www.nrel.gov/docs/legosti/old/7668.pdf.
+
         :param int length: length of tm2 file (number of records, typically 1 record per hour for a year, so 8760)
         """
         self.length = length
         self.header = HeaderRecord(lat, long, time_zone)
-        self.header.update_header()
+        self.header.update()
         self.records = [DataRecord() for _ in range(self.length)]
 
     def write(self, data: dict, start: int = 0) -> None:
-        """Write data into records.
+        """Write weather dataset into records.
 
         :param dict data: dict with parameter as key and data as list
         :param int start: starting position of the dataset within the tm2 file (0-8759)
@@ -37,8 +40,8 @@ class TMY2:
         for record in self.records:
             print("".join(record.data))
 
-    def export(self, path:str)-> None:
-        """Export all records to designated path.
+    def export(self, path: str) -> None:
+        """Export all records to designated path as tm2 file.
 
         :param str path: output path, to receive a tm2 file, the file extension should be .tm2
         """
@@ -46,27 +49,26 @@ class TMY2:
         with open(path, 'w') as f:
             f.write("".join(self.header.data))  # write header
             for record in self.records:
-                f.write("".join(record.data))  # write record
                 f.write('\n')  # new line
+                f.write("".join(record.data))  # write record
 
 
 class Record:
     def __init__(self, file_format: dict, line_len: int) -> None:
         """Initialize record.
 
-        The term "record" defines an hourly data entry with multiple sensor values. The format is defined in "The User's
-        Manual for TMY2s". Thereby, the first record is always a header with its own format, also described in the
-        manual.
+        Regarding the tmy2 (Typical Meteorological Years) format, the term "record" defines a single line. The first
+        record is reserved for the header, all others a data records in hourly resolution with multiple sensor entries.
 
-        :param dict file_format:
-        :param int line_len:
+        :param dict file_format: tmy2 format information containing the positions of individual elements within a record
+        :param int line_len: length of the record (number of characters)
         """
 
         self.data = list(' ' * line_len)  # data consisting of a list of characters
         self.format = file_format  # tmy2 format information about header and data element, as dict
 
     def set_values(self, values: dict) -> None:
-        """Writes multiple key-value pairs inside the record.
+        """Writes multiple key-value pairs into the record.
 
         Accepts a dict of key-value pairs (key must correspond to the name of an element in the tmy2 format) and writes
         the value at the designated position inside the record.
@@ -100,15 +102,18 @@ class Record:
 
 class HeaderRecord(Record):
     def __init__(self, lat: float, long: float, time_zone: int,
-                 wban: str = '00000', city: str = 'unknown', state: str = 'ZZ', elevation: int = 0):
+                 wban: str = '00000', city: str = 'unknown', state: str = 'ZZ', elevation: int = 0) -> None:
+        """Initialize header record."""
+
         # initialize as header record
         super().__init__(HEADER_ELEMENTS_POS, 59)
 
-        lat_min, long_min = get_lat_long_minutes(lat, long)
         self.wban = wban
         self.city = city.ljust(22)
         self.state = state
         self.timezone = str(int(time_zone)).rjust(3)
+
+        lat_min, long_min = get_lat_long_minutes(lat, long)
         self.latitude = ('N'
                          + f' {int(lat):02d}'  # degrees
                          + f' {int(lat_min):02d}'  # minutes
@@ -117,9 +122,12 @@ class HeaderRecord(Record):
                           + f' {int(long):02d}'  # degrees
                           + f' {int(long_min):02d}'  # minutes
                           )
+
         self.elevation = f'{elevation:03d}'
 
-    def update_header(self):
+    def update(self) -> None:
+        """Updates header record according to attribute values of the instance."""
+
         for key in self.format:
             # start and end of the position designated to the element corresponding to the passed key
             start_pos = self.format[key]['value'][0] - 1
@@ -131,14 +139,14 @@ class HeaderRecord(Record):
             self.data[start_pos:end_pos] = getattr(self, key)
 
 
-def get_lat_long_minutes(lat: float, long: float):
-    """
+def get_lat_long_minutes(lat: float, long: float) -> (int, int):
+    """Get latitude/longitude minutes from float latitude/longitude values.
 
     :param float lat: latitude
     :param float long: longitude
     :return:
         - lat_min - latitude minutes
-        -long_min - longitude minutes
+        - long_min - longitude minutes
     """
 
     lat_int = int(lat)
@@ -151,7 +159,7 @@ def get_lat_long_minutes(lat: float, long: float):
 
 class DataRecord(Record):
     def __init__(self):
-        """"""
+        """Initialize data record."""
 
         # initialize as data record
         super().__init__(DATA_ELEMENTS_POS, 142)
@@ -160,10 +168,13 @@ class DataRecord(Record):
         self.source_flag = '?'
         self.uncertainty_flag = '0'
 
-        self.reset()  # fill data with "missing data" values
+        self.reset()  # fill record with "missing data" values
 
-    def reset(self):
-        """Reset data record."""
+    def reset(self) -> None:
+        """Reset data record.
+
+        Fills record with "missing data" values, "unknown" source flag and "not applicable" uncertainty flag.
+        """
 
         for key in self.format:
             if key in ['year', 'month', 'day', 'hour']:
@@ -176,11 +187,13 @@ class DataRecord(Record):
             entry_len = end_pos - start_pos  # length of entry
             missing_data_entry = '9' * entry_len
 
-            # write entry into record - right-aligned and with 0s at unused digits
+            # write entry into record - right-aligned and with leading zeros
             self.data[start_pos:end_pos] = missing_data_entry
 
+            # set source flag
             if 'source_flag' in self.format[key].keys():
                 self.data[self.format[key]['source_flag'] - 1] = self.source_flag
 
+            # set uncertainty flag
             if 'uncertainty_flag' in self.format[key].keys():
                 self.data[self.format[key]['uncertainty_flag'] - 1] = self.uncertainty_flag
